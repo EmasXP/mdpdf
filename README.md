4869013e3bbac53bded7358ed37776cfe706a419
Magnus Karlsson <--local>
2024-04-06 10:14:17 +0200

    Adding better meta data block support.

--- README.md
+++ README.md
@@ -1,3 +1,55 @@
 # mdpdf
 
-This is just a personal project that I use to convert Markdown to PDF. It does that using Pandoc and a customized LaTeX template. It generates high quality PDF files due to this approach, simply because Pandoc and LaTeX are awesome.
+This is just a personal project that I use to convert Markdown to PDF. It does that using Pandoc and a slightly customized LaTeX template. It generates high quality PDF files due to this approach, simply because Pandoc and LaTeX are awesome.
+
+## Starting the service
+
+The only thing to remember is to have the correct working directory whens tarting the service. You need to start the service from the directory where `template.latex` is located. Otherwise `template.latex` will not be found by the service during run time. For example:
+
+```shell
+cd mdpdf
+./mdpdf
+```
+
+## Firejail
+
+The `pandoc` command is called with `--sandbox=true`, but to increase the sandbox even further, you can use `firejail`.
+
+You will need to create a firejail profile, in this example I am going to call it `mdpdf.profile`:
+
+```plain
+whitelist /absolute/path/to/mdpdf/template.latex
+```
+
+This will work if you have put the `mdpdf` binary on your PATH (which is accessible by the root user.)
+
+If you don't want to add `mdpdf` to your PATH, then you need to add this additional line to `mdpdf.profile`:
+
+```plain
+whitelist /absolute/path/to/mdpdf/mdpdf
+```
+
+If you want to add `mdpdf` to your PATH, you can do that like this:
+
+```shell
+cp mdpdf /usr/bin/
+```
+
+(and here `mdpdf` is the binary file, not the folder)
+
+And now you can start the service:
+
+```shell
+sudo firejail --profile=mdpdf.profile /absolute/path/to/mdpdf/mdpdf
+```
+
+If you have `mdpdf` on your PATH, you don't need to specify the absolute path, simply saying `mdpdf` is enough.
+
+## Supervisor configuration example
+
+```ini
+[program:mdpdf]
+command=firejail --profile=mdpdf.profile mdpdf
+directory=/absolute/path/to/mdpdf/
+user=root
+```
--- main.go
+++ main.go
@@ -18,7 +18,16 @@ const INDEX = `
 	<body>
 
 		<form method="post">
-			<textarea name="md" style="width:100%; height: 400px"></textarea>
+			<textarea name="md" style="width:100%; height: 400px">---
+papersize: a4
+pagestyle: empty
+fontsize: 12pt
+linestretch: 1.2
+geometry: margin=6.25em
+large-h1: true
+---
+
+</textarea>
 			<button>To PDF</button>
 		</form>
 
@@ -33,12 +42,92 @@ const INDEX = `
 		</p>
 
 		<p>
-			More technical stuff: The LaTeX engine used is <a href="https://tug.org/xetex/">XeTeX</a>, the site itself is built using <a href="https://go.dev/">Go</a>. It's amazing how much tech is needed for such a small web site.
+			More technical stuff: The LaTeX engine used is <a href="https://tug.org/xetex/">XeTeX</a>, the site itself is built using <a href="https://go.dev/">Go</a>.
 		</p>
 
 		<p>
 			The source code can be found here: <a href="https://github.com/EmasXP/mdpdf">https://github.com/EmasXP/mdpdf</a>
 		</p>
+
+		<h2>The meta block</h2>
+
+		<p>
+		One can add a meta data block in the beginning of the Markdown source, like this:
+		</p>
+
+		<pre><code>---
+Meta data goes here
+---
+The document starts here.</code></pre>
+
+		<p>
+			I've added a predefined meta data block that I believe works well for Markdown files (at least, this is how I personally think the Markdown files should look when printed on paper.)
+		</p>
+
+		<p>
+			As an example, one can add a title like this:
+		</p>
+
+		<pre><code>---
+title: My document
+---</code></pre>
+
+		<p>
+			Vertical space is added between paragraphs by default. That can be changed to use indentation instead:
+		</p>
+
+		<pre><code>---
+title: My document
+indent: true
+---</code></pre>
+
+		<p>
+			I've kept <code>title</code> in this example just to show you how it looks when several variables are defined.
+		</p>
+
+		<p>
+			To specify the language of the document, use the <code>lang</code> variable:
+		</p>
+
+		<pre><code>---
+lang: en-GB
+---</code></pre>
+
+		<p>
+			More variables are found here: <a href="https://pandoc.org/chunkedhtml-demo/6.2-variables.html">https://pandoc.org/chunkedhtml-demo/6.2-variables.html</a>.
+		</p>
+
+		<p>
+			The <code>mainfont</code> is hard-coded and cannot be changed.
+		</p>
+
+		<h3>Additional variables</h3>
+
+		<p>
+			Two additional variables has been added:
+		</p>
+
+		<h4>large-h1</h4>
+
+		<p>
+			Since Markdown most often does not have a title variable, but uses the H1 (<code>#</code>) as such, the <code>large-h1</code> can be used to simply make the H1 larger:
+		</p>
+
+		<pre><code>---
+large-h1: true
+---</code></pre>
+
+		<h4>additional</h4>
+
+		<p>
+			Another way of achieving a larger H1 is to use the <code>additional</code> variable:
+		</p>
+
+		<pre><code>---
+additional: \usepackage{sectsty}
+  \sectionfont{\fontsize{21}{21}\selectfont}
+---</code></pre>
+
 	</body>
 </html>
 `
@@ -51,24 +140,18 @@ func main() {
 	http.HandleFunc("POST /", func(w http.ResponseWriter, r *http.Request) {
 		command := exec.Command(
 			"pandoc",
+			"--sandbox=true",
 			"-f",
 			"gfm",
 			"--to",
 			"pdf",
-			"-V",
-			"papersize:a4",
-			"-V",
-			"pagestyle=empty",
-			"-V",
-			"linestretch=1.2",
 			"--template",
 			"template.latex",
 			"-V",
-			"fontsize:12pt",
-			"-V",
-			"documentclass=scrartcl",
-			"-V",
 			"mainfont=EB Garamond",
+			// Trying out sans-serif fonts for a future sans-serif version:
+			//"mainfont=Inter",
+			//"mainfont=Carlito",
 			"--pdf-engine",
 			"xelatex",
 		)
