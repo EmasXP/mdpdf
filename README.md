136b64197722c4498d9650f85d51c9ca2fef8a66
Magnus Karlsson <--local>
2025-09-17 09:28:55 +0200

    Creating a standalone CLI version, and removing the custom template.

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # mdpdf
 
-This is just a personal project that I use to convert Markdown to PDF. It does that using Pandoc and a slightly customized LaTeX template. It generates high quality PDF files due to this approach, simply because Pandoc and LaTeX are awesome.
+This is just a personal project that I use to convert Markdown to PDF. It does that using Pandoc and the EB Garamond 12 font. It generates high quality PDF files due to this approach, simply because Pandoc and LaTeX are awesome.
 
 ## Installing the fonts
 
 If you don't already have EB Garamond 12 installed, go to this address https://github.com/octaviopardo/EBGaramond12/ and download the font files, preferably the OTF versions.
 
-You need make sure that the fonts are accessible by the user that is running the service, and that's easiest by putting them in the `/usr/share/fonts` folder, for example:
+You need to make sure that the fonts are accessible by the user that is running the service, and that's easiest by putting them in the `/usr/share/fonts` folder, for example:
 
 ```sh
 sudo mkdir /usr/share/fonts/EBGaramond12
@@ -15,7 +15,7 @@ sudo mv EBGaramond-*.otf /usr/share/fonts/EBGaramond12/
 
 ## Starting the service
 
-The only thing to remember is to have the correct working directory when starting the service. You need to start the service from the directory where `template.latex` is located. Otherwise `template.latex` will not be found by the service during run time. For example:
+It's rather easy:
 
 ```sh
 cd mdpdf
@@ -26,13 +26,11 @@ cd mdpdf
 
 The `pandoc` command is called with `--sandbox=true`, but to increase the sandbox even further, you can use `firejail`.
 
-You will need to create a firejail profile, in this example I am going to name it `mdpdf.profile`:
-
-```plain
-whitelist /absolute/path/to/mdpdf/template.latex
+```sh
+sudo firejail mdpdf
 ```
 
-This will work if you have put the `mdpdf` binary on your PATH (which is accessible by the root user.)
+This is all you need do if you have put the `mdpdf` binary on your PATH (which is accessible by the root user.)
 
 If you don't want to add `mdpdf` to your PATH, then you need to add this additional line to `mdpdf.profile`:
 
@@ -40,7 +38,7 @@ If you don't want to add `mdpdf` to your PATH, then you need to add this additio
 whitelist /absolute/path/to/mdpdf/mdpdf
 ```
 
-You can do that like this if you want to add `mdpdf` to your PATH:
+You can do like this if you want to add `mdpdf` to your PATH:
 
 ```sh
 cp mdpdf /usr/bin/
@@ -54,13 +52,33 @@ And now you can start the service:
 sudo firejail --profile=mdpdf.profile /absolute/path/to/mdpdf/mdpdf
 ```
 
-If you have `mdpdf` on your PATH, you don't need to specify the absolute path, simply saying `mdpdf` is enough.
-
 ## Supervisor configuration example
 
 ```ini
 [program:mdpdf]
 command=firejail --profile=mdpdf.profile mdpdf
-directory=/absolute/path/to/mdpdf/
 user=root
 ```
+
+## Using as a CLI tool
+
+```
+Usage: ./mdpdf [options] <input> [output]
+  -template string
+        Path or name of the LaTeX template file (default "default")
+```
+
+Example:
+
+```sh
+mdpdf my-document.md
+```
+
+This will render a `my-document.pdf`.
+
+To specify the output file:
+
+```sh
+mdpdf my-document.md output-file.pdf
+```
+
--- main.go
+++ main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bytes"
+	"flag"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 )
 
@@ -15,6 +19,9 @@ const INDEX = `
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<title>MD</title>
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=EB+Garamond:ital,wght@0,400..800;1,400..800&family=Inconsolata:wght@200..900&display=swap" rel="stylesheet">
 	<body>
 
 		<form method="post">
@@ -24,115 +31,233 @@ pagestyle: empty
 fontsize: 12pt
 linestretch: 1.2
 geometry: margin=6.25em
-large-h1: true
+header-includes:
+  - \pagenumbering{gobble}
+  - \usepackage{titling}
+  - \setlength{\droptitle}{-3.88em}
+  - \pretitle{\fontsize{21}{21}\selectfont\bfseries}
+  - \posttitle{\par\vspace{-3.1em}}
 ---
 
 </textarea>
-			<button>To PDF</button>
+			<button>Convert Markdown to PDF</button>
 		</form>
 
-		<h2>What's this?</h2>
+		<div id="information">
+			<h1>What's this?</h1>
 
-		<p>
-			This is just a web page to convert <a href="https://en.wikipedia.org/wiki/Markdown">Markdown</a> into PDF. It does that using <a href="https://pandoc.org/">Pandoc</a> and a customized <a href="https://en.wikipedia.org/wiki/LaTeX">LaTeX</a> template. The most notable change is the use of the <a href="https://github.com/octaviopardo/EBGaramond12/">EB Garamond 12</a> font. Pandoc and LaTeX generates super crisp PDF files. Before I used this approach I converted the Markdown to HTML, and used a web browser to "print" the page to a PDF file. For some reason the fonts are rendered more blurry when taking that route.
-		</p>
+			<p>
+				This is just a web page to convert <a href="https://en.wikipedia.org/wiki/Markdown">Markdown</a> into PDF. It does that using <a href="https://pandoc.org/">Pandoc</a> and the <a href="https://github.com/octaviopardo/EBGaramond12/">EB Garamond 12</a> font. Pandoc and LaTeX generates super crisp PDF files. Before I used this approach I converted the Markdown to HTML, and used a web browser to "print" the page to a PDF file. For some reason the fonts are rendered more blurry when taking that route.
+			</p>
 
-		<p>
-			This web page is hosted on an old Raspberry Pi, so it is rather slow. Even though this is intended for my personal use, you are free to use it too.
-		</p>
+			<p>
+				This web page is hosted on an old Raspberry Pi, so it is rather slow. Even though this is intended for my personal use, you are free to use it too.
+			</p>
 
-		<p>
-			More technical stuff: The LaTeX engine used is <a href="https://tug.org/xetex/">XeTeX</a>, the site itself is built using <a href="https://go.dev/">Go</a>.
-		</p>
+			<p>
+				More technical stuff: The LaTeX engine used is <a href="https://tug.org/xetex/">XeTeX</a>, the site itself is built using <a href="https://go.dev/">Go</a>.
+			</p>
 
-		<p>
-			The source code can be found here: <a href="https://github.com/EmasXP/mdpdf">https://github.com/EmasXP/mdpdf</a>
-		</p>
+			<p>
+				The source code can be found here: <a href="https://github.com/EmasXP/mdpdf">https://github.com/EmasXP/mdpdf</a>
+			</p>
 
-		<h2>The meta block</h2>
+			<h2>The meta block</h2>
 
-		<p>
-		One can add a meta data block in the beginning of the Markdown source, like this:
-		</p>
+			<p>
+				One can add a meta data block in the beginning of the Markdown source, like this:
+			</p>
 
-		<pre><code>---
+			<pre><code>---
 Meta data goes here
 ---
-The document starts here.</code></pre>
+	The document starts here.</code></pre>
 
-		<p>
-			I've added a predefined meta data block that I believe works well for Markdown files (at least, this is how I personally think the Markdown files should look when printed on paper.)
-		</p>
+			<p>
+				I've added a predefined meta data block that I believe works well for Markdown files (at least, this is how I personally think the Markdown files should look when printed on paper.)
+			</p>
 
-		<p>
-			As an example, one can add a title like this:
-		</p>
+			<h3>Defining the title of the document</h3>
 
-		<pre><code>---
+			<p>
+				That is done using the <code>title</code> variable:
+			</p>
+
+			<pre><code>---
 title: My document
 ---</code></pre>
 
-		<p>
-			Vertical space is added between paragraphs by default. That can be changed to use indentation instead:
-		</p>
+			<h4>Markdown title</h4>
 
-		<pre><code>---
-title: My document
+			<p>
+				Markdown files commonly use the H1 (<code># Title</code>) for the document title. If that is the case, my suggestion is to use the <code>title</code> variable in the meta block instead. That way the title will be typeset in a larger font size.
+			</p>
+
+			<p>
+				A <code>title</code> is rendered in the center of the page, and has large top and bottom margins. Markdown files are commonly <i>not</i> rendered this way. If you want to make the <code>title</code> look more like a regular H1, add this to the meta block:
+			</p>
+
+			<pre><code>---
+header-includes:
+- \usepackage{titling}
+- \setlength{\droptitle}{-3.88em}
+- \pretitle{\fontsize{21}{21}\selectfont\bfseries}
+- \posttitle{\par\vspace{-3.1em}}
+---</code></pre>
+
+			<p>
+				This example will make the title more distinct than an ordinary H1, which is suitable for a document title.
+			</p>
+
+			<h3>Paragraph spacing</h3>
+
+			<p>
+				Vertical space is added between paragraphs by default. That can be changed to use indentation instead:
+			</p>
+
+			<pre><code>---
 indent: true
 ---</code></pre>
 
-		<p>
-			I've kept <code>title</code> in this example just to show you how it looks when several variables are defined.
-		</p>
+			<h3>Language of the document</h3>
 
-		<p>
-			To specify the language of the document, use the <code>lang</code> variable:
-		</p>
+			<p>
+				To specify the language of the document, use the <code>lang</code> variable:
+			</p>
 
-		<pre><code>---
+			<pre><code>---
 lang: en-GB
 ---</code></pre>
 
-		<p>
-			More variables are found here: <a href="https://pandoc.org/chunkedhtml-demo/6.2-variables.html">https://pandoc.org/chunkedhtml-demo/6.2-variables.html</a>.
-		</p>
+			<h3>Removal of page numbers</h3>
 
-		<p>
-			The <code>mainfont</code> is hard-coded and cannot be changed.
-		</p>
+			<p>
+				When using <code>pagestyle: empty</code>, page numbers are removed. However, the first page will still have a page number. To remove that too, one can add the following:
+			</p>
 
-		<h3>Additional variables</h3>
-
-		<p>
-			Two additional variables has been added:
-		</p>
-
-		<h4>large-h1</h4>
-
-		<p>
-			Since Markdown most often does not have a title variable, but uses the H1 (<code>#</code>) as such, the <code>large-h1</code> can be used to simply make the H1 larger:
-		</p>
-
-		<pre><code>---
-large-h1: true
+			<pre><code>---
+header-includes:
+- \pagenumbering{gobble}
 ---</code></pre>
 
-		<h4>additional</h4>
+			<h3>More variables</h3>
 
-		<p>
-			Another way of achieving a larger H1 is to use the <code>additional</code> variable:
-		</p>
+			<p>
+				More variables are found here: <a href="https://pandoc.org/chunkedhtml-demo/6.2-variables.html">https://pandoc.org/chunkedhtml-demo/6.2-variables.html</a>.
+			</p>
 
-		<pre><code>---
-additional: \usepackage{sectsty}
-  \sectionfont{\fontsize{21}{21}\selectfont}
----</code></pre>
+			<p>
+				The <code>mainfont</code> is hard-coded and cannot be changed.
+			</p>
+		</div>
+
+		<style>
+		<!--
+		body {
+			font-family: 'EB Garamond', serif;
+		}
+		#information {
+			max-width: 60em;
+			margin: auto;
+			padding: 1em;
+			margin-bottom: 4em;
+		}
+		code, textarea {
+			font-family: 'Inconsolata', monospace;
+			font-size: 0.85em;
+		}
+		pre {
+			background: #f8f8f8;
+			padding: 0.6em 1em;
+			overflow-x: auto;
+			border-radius: 0.5em;
+			border: 1px solid #eeeeee;
+		}
+		h2, h3, h4 {
+			margin-top: 2em;
+		}
+		button {
+			padding: 0.5em 1em;
+		}
+		-->
+		</style>
 
 	</body>
 </html>
 `
 
 func main() {
+	// Define the optional flag
+	templatePath := flag.String("template", "default", "Path or name of the LaTeX template file")
+
+	// Custom usage function to show usage info
+	flag.Usage = func() {
+		fmt.Fprintf(flag.CommandLine.Output(), "Usage: %s [options] <input> [output]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	// Parse the flags
+	flag.Parse()
+
+	// Get positional arguments
+	args := flag.Args()
+	if len(args) < 1 {
+		web(*templatePath)
+	}
+
+	input := args[0]
+
+	var output string
+	if len(args) >= 2 {
+		output = args[1]
+	} else {
+		// Derive output from input
+		ext := filepath.Ext(input)
+		if ext == ".md" {
+			output = strings.TrimSuffix(input, ".md") + ".pdf"
+		} else {
+			output = input + ".pdf"
+		}
+	}
+
+	cli(input, output, *templatePath)
+}
+
+func cli(input, output, templatePath string) {
+	fmt.Println(templatePath)
+	command := exec.Command(
+		"pandoc",
+		input,
+		"--sandbox=false",
+		"-o",
+		output,
+		"-f",
+		"markdown",
+		"--to",
+		"pdf",
+		"--template",
+		templatePath,
+		"-V",
+		"mainfont=EB Garamond",
+		// Trying out sans-serif fonts for a future sans-serif version:
+		//"mainfont=Inter",
+		//"mainfont=Carlito",
+		"--pdf-engine",
+		"xelatex",
+	)
+
+	var stderr bytes.Buffer
+	command.Stderr = &stderr
+
+	_, err := command.Output()
+	if err != nil {
+		error, _ := io.ReadAll(&stderr)
+		log.Println(string(error))
+		return
+	}
+}
+
+func web(templatePath string) {
 	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		io.WriteString(w, INDEX)
 	})
@@ -142,11 +267,11 @@ func main() {
 			"pandoc",
 			"--sandbox=true",
 			"-f",
-			"gfm",
+			"markdown",
 			"--to",
 			"pdf",
 			"--template",
-			"template.latex",
+			templatePath,
 			"-V",
 			"mainfont=EB Garamond",
 			// Trying out sans-serif fonts for a future sans-serif version:
